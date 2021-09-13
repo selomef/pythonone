14ba11a048be55305407b376e0057206d780707a
selomef <90533406+selomef@users.noreply.github.com>
2021-09-12 22:13:37 -0700

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,43 +8,38 @@
 
 def welcome_assignment_answers(question):
 # Press the green button in the gutter to run the script.
- question = "Are encoding and encryption the same? - Yes/No"
- print(welcome_assignment_answers(question))
-answer = "NO"
-print(answer)
-debug_question = "Are encoding and encryption the same? - Yes/No"
-print(welcome_assignment_answers(debug_question))
-question = "Is it possible to decrypt a message without a key? - Yes/No"
+
+ if question == "Is it possible to decrypt a message without a key? - Yes/No":
+        answer = "NO"
+        return(answer)
+ elif question == "Are encoding and encryption the same? - Yes/No":
+        answer = "NO"
+        return(answer)
+ elif question == "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?":
+      answer = "mTLS"
+      return(answer)
+ elif question == "Is it possible to decode a message without a key? - Yes/No":
+      answer = "Yes"
+      return(answer)
+ elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+       answer = "NO"
+       return(answer)
+ elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
+       answer = "42b76fe51778764973077a5a94056724"
+       return(answer)
+ elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+       answer = "NO"
+       return(answer)
+ elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
+       answer = "5"
+       return(answer)
+ elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
+       answer = "4"
+       return(answer)
+ else:
+    return(0)
+question = ("What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code")
 print(welcome_assignment_answers(question))
-answer = "NO"
-print(answer)
-question = "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?"
-print(welcome_assignment_answers(question))
-answer = "mTLS"
-print(answer)
-question = "Is it possible to decode a message without a key? - Yes/No"
-print(welcome_assignment_answers(question))
-answer = "Yes"
-print(answer)
-question = "Is a hashed message supposed to be un-hashed? - Yes/No"
-print(welcome_assignment_answers(question))
-answer = "NO"
-print(answer)
-question = "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code"
-print(welcome_assignment_answers(question))
-answer = "42b76fe51778764973077a5a94056724"
-print(answer)
-question = "Is MD5 a secured hashing algorithm? - Yes/No"
-print(welcome_assignment_answers(question))
-answer = "NO"
-print(answer)
-question = "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number"
-print(welcome_assignment_answers(question))
-answer = "5"
-print(answer)
-question = "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number"
-print(welcome_assignment_answers(question))
-answer = "4"
-print(answer)
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
